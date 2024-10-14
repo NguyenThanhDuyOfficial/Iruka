d02aeb5cbcfa2bf3f9e97d04a64d9d4a170f83fa
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2024-10-14 10:46:41 +0700

    Add package install command.

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
+# Install yay
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+rm -rf yay
+
+# Install packages.
+yay -S pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse hyprland-git ttf-cascadia-code-nerd stow sudo alacritty fish starship neovim wl-clipboard rofi-wayland qutebrowser
+
 stow -R configs
 
 # Fish
 fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && rm ~/.config/fish/functions/fisher.fish && fisher install jorgebucaran/fisher'
 fish -c 'fisher install catppuccin/fish'
 sudo pacman -S starship
-
-
-

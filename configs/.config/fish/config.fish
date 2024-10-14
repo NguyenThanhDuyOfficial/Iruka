04098040ed0eb058d7349547bfd58c4b50c59398
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2024-10-14 10:03:42 +0700

    Add git abbreviations, disable greeting, change to vi key bindings and set nvim default editor.

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -1,3 +1,13 @@
+abbr --add gs git status
+abbr --add gb git branch
+abbr --add gc git checkout
+abbr --add ga git add
+abbr --add gcm git commit -m
+abbr --add gp git push
+
+set -U fish_greeting
+set -U fish_key_bindings fish_vi_key_bindings
+set -Ux EDITOR nvim
+
 source ~/.config/theme/fish.fish
-set -U FISHER_NO_EDIT 1
 starship init fish | source

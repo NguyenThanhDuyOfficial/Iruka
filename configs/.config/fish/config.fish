cc3c770e82ac0f7c20e70fadc5f7ad6b56ba808e
NguyenThanhDuyOfficial <nguyenthanhduyofficial@gmail.com>
2024-10-14 09:29:38 +0700

    Add Starship plugin.

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -1,2 +1,3 @@
 source ~/.config/theme/fish.fish
 set -U FISHER_NO_EDIT 1
+starship init fish | source
--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ stow -R configs
 
 # Fish
 fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && rm ~/.config/fish/functions/fisher.fish && fisher install jorgebucaran/fisher'
+fish -c 'fisher install catppuccin/fish'
+sudo pacman -S starship
 
 
 
